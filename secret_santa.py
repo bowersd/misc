e44f011842217941d41bff659dfd13fb8dc97e94
sautedman <bowersd@ucla.edu>
2018-11-21 23:41:38 -0700

    cli for when you need a quick secret santa assignment

---
+++ secret_santa.py
@@ -0,0 +1,13 @@
+import random
+import sys
+
+def shuffle_rtn(*items):
+    a = [x for x in items]
+    random.shuffle(a)
+    return a
+
+def circle(*items):
+    return [(items[i], items[i+1]) for i in range(len(items)-1)] + [(items[-1], items[0])]
+
+if __name__ == "__main__":
+    for x in circle(*shuffle_rtn(*sys.argv[1:])): print x
