bda1214d1e5ddd3595bf24d2af51a94bbbbf265b
sautedman <bowersd@ucla.edu>
2018-11-24 11:26:30 -0700

    python3 print statement

--- secret_santa.py
+++ secret_santa.py
@@ -10,4 +10,4 @@ def circle(*items):
     return [(items[i], items[i+1]) for i in range(len(items)-1)] + [(items[-1], items[0])]
 
 if __name__ == "__main__":
-    for x in circle(*shuffle_rtn(*sys.argv[1:])): print x
+    for x in circle(*shuffle_rtn(*sys.argv[1:])): print(x)
